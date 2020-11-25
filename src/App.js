225004a6d3964043f2a1fcda8e7ea88c0376baa9
Nishith A <nishithza@gmail.com>
2020-11-25 09:26:03 +0530

    image card

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
 import React, { useState, useEffect} from 'react';
-
+import ImageCard from './components/imageCard'
 function App() {
   const [images, setImages] = useState([]);
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
 
   useEffect(()=>{
-    //https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo
-    fetch(`https://pixabay.com/api/?key=19256212-e3fbe15d3b137a71c410dc89e&q=yellow+flowers&image_type=photo`)
+    
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`)
       .then(res=> res.json())
-      .then(data =>console.log(data))
+      .then(data =>{
+        setImages(data.hits);
+        setIsLoading(false);
+      })
       .catch(err =>console.log(err))
   })
 
 
   return (
     <div >
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-purple-500 text-xl mb-2">
-            Photo by John Doe
-          </div>
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads:</strong>
-            300
-          </li>
-          <li>
-            <strong>Likes:</strong>
-            400
-          </li>
-        </ul>
-
-     
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
-        </div>
-      </div>
+      <ImageCard />
     </div>
   );
 }
